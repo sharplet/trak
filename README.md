44aa8fd51526054df0dcf60d02cedd6253ca867e
Adam Sharp <adsharp@me.com>
2011-09-02 15:58:30 +1000

    v0.2: Automatically calculate the day's start time
    Added:
    
    - Tracker now automatically calculates the time you started your day
      based on your first log for that day. This is stored in the time log
      after the date, and used to calculate how far into the day you have
      tracked time.
    
    Changed:
    
    - Everything after the first argument (the amount of time) is now
      considered to be the task name (i.e., the task is no longer truncated
      to the first word).
    - Formatting improvements

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 # Tracker --- track blocks of time from the command line
 
-Tracker.pl, v0.1 (Sep 1, 2011)  
+Tracker.pl, v0.2 (Sep 2, 2011)  
 Written by Adam Sharp  
 
 ## Description
 
-Tracker is a utility for Mac OS X that allows you to quickly make a record of how much time you've spent on various tasks throughout the day.
+Tracker is a utility for Mac OS X that allows you to quickly make a record of
+how much time you've spent on various tasks throughout the day.
 
-Work logs are stored in `/Users/yourusername/Documents/Tracker/` with the format `YEAR-MONTH-DAY-time-log.txt`.
+Work logs are stored in `/Users/yourusername/Documents/Tracker/` with the format
+`YEAR-MONTH-DAY-time-log.txt`.
 
 An example work log that Tracker will create:
 
-    2011-09-01
+    2011-09-01 9:00
     30: nap
     45: procrastinate
     30: uni
@@ -25,11 +27,13 @@ Either:
 
 OR
 
-- Copy Tracker.pl to the destination of your choice and create a symlink to tracker in your path, e.g.:
+- Copy Tracker.pl to the destination of your choice and create a symlink to
+  tracker in your path, e.g.:
 
         $ ln -s /path/to/Tracker.pl /usr/bin/track
 
-Adam recommends either creating a symlink called `track` or if you just put Tracker.pl in your `PATH` then create it as an alias like so:
+Adam recommends either creating a symlink called `track` or if you just put
+Tracker.pl in your `PATH` then create it as an alias like so:
 
     $ alias track=Tracker.pl
 
@@ -43,35 +47,56 @@ If you don't happen to have Tracker.pl you could also do this:
     Tracker.pl -r                        # => reporting
     Tracker.pl -l                        # => list today's work
 
-Where `<minutes>` is an integer signifying how much time has been spent and `<description>` is a string containing a brief description of the activity (one word is best, but can be any length of string).
+Where `<minutes>` is an integer signifying how much time has been spent and
+`<description>` is a string containing a brief description of the activity.
 
-Make sure to use
+You can use either
 
     $ Tracker.pl 30 "Foo bar"
     
-and not
+or
 
     $ Tracker.pl 30 Foo bar
 
-as the description will be truncated to "Foo".
+as everything after the first argument is considered the name of the task.
 
 ## To do
 
-- Make the personal time search more configurable by putting keywords to search in an array
-- Reporting feature (calculate total work done for each task)
+- Make the personal time search more configurable by putting keywords to search
+  in an array
+- Calculate total work done for each task
 - Report on days other than today
 - More robust command line args
 - Add a `-h` usage/help switch
-- More time formats than just minutes, e.g., handle `###<denom>` where `##` is a decimal number rounded to the nearest 15 minutes (1, 1.5, 0.25) and `<denom>` is some modifier such as `h/hr/hour`, `m/min/minute`. Also, no `<denom>` defaults to minutes.
-- Offer support for an environment variable that defines the default start time (currently hard coded to 8:00 AM)
+- More time formats than just minutes, e.g., handle `###<denom>` where `##` is a
+  decimal number rounded to the nearest 15 minutes (1, 1.5, 0.25) and `<denom>`
+  is some modifier such as `h/hr/hour`, `m/min/minute`. Also, no `<denom>`
+  defaults to minutes.
+- Refactor
 
 ## Change log
+
+### v0.2
+
+Added:
+
+- Tracker now automatically calculates the time you started your day based on
+  your first log for that day. This is stored in the time log after the date,
+  and used to calculate how far into the day you have tracked time.
+
+Changed:
+
+- Everything after the first argument (the amount of time) is now considered to
+  be the task name (i.e., the task is no longer truncated to the first word).
+- Formatting improvements
 
 ### v0.1
 
 Added:
 
-- Tracker.pl -l will now tell you what time you've logged time until. For example, if your start time is 8:00 AM and you've logged 3 hours, it will output the following:
+- Tracker.pl -l will now tell you what time you've logged time until. For
+  example, if your start time is 8:00 AM and you've logged 3 hours, it will
+  output the following:
     
         Hours logged until 11:00 AM (since 8:00 AM).
 
--- Tracker.pl
+++ Tracker.pl
@@ -29,8 +29,11 @@ if ($ARGV[0] eq "-r" || $ARGV[0] eq "-l") {
         @file = <REPORTFILE>;
         $end = @file;
         $personal = 0, $total = 0;
+        @firstLine = split(/\ /, $file[0]);
+        my $startTime = $firstLine[1];
         print "# Today's logged work\n";
         foreach $line (@file[1..$end-1]) {
+            chomp($line);
             $line =~ /^(\d+)\:\s+(.+)$/;
             $minutes = $1;
             $text = $2;
@@ -40,51 +43,116 @@ if ($ARGV[0] eq "-r" || $ARGV[0] eq "-l") {
             else {
                 $personal += $minutes;
             }
-            print "=> ".$minutes."m $text\n";
+            print "=> ".$minutes."m: $text\n";
         }
         if ($total > 0) { print "Work time = " . $total/60 . "h\n"; }
         if ($personal > 0) { print "Personal time = " . $personal/60 . "h\n"; }
-        $newTimeString = to12HourTime(newTimeWithMinutes("8:00", $total + $personal));
-        print "Hours logged until ".$newTimeString." (since 8:00 AM).\n";
+        $newTimeString = to12HourTime(newTimeWithMinutes($startTime, $total + $personal));
+        print "Hours logged until ".$newTimeString." (since ".to12HourTime($startTime).").\n";
     }
     else {
         print STDERR "No time log for today. Track some time first.\n";
     }
 }
 else {
+    # process arguments
+    my $minutes = $ARGV[0];
+    my $message = join(" ", @ARGV[1..@ARGV-1]);
+    
     # open the output file
     if (!-e $filename) {
         open TIMELOG, ">> $filename" or die "Couldn't open $filename: $!";
-        print TIMELOG "$fdate\n";
+        my $currentTimeInMinutes = timeToMinutes(currentTimeFormatted());
+        my $startTime = minutesToTime(nearest15Minutes($currentTimeInMinutes - $minutes));
+        print TIMELOG "$fdate $startTime\n";
     }
     else {
         open TIMELOG, ">> $filename" or die "Couldn't open $filename: $!";
     }
     
-    # process arguments
-    ($minutes, $message) = (@ARGV)[0,1];
+    # print the logged time
     print TIMELOG "$minutes: $message\n";
 }
 
+
+
+
+
+
+
 sub newTimeWithMinutes
 {
-    $len = @_;
+    my $len = @_;
     if ($len == 2) {
-        @startTime = split(":", $_[0]);
-        $startMinutes = $startTime[0] * 60 + $startTime[1];
-        $endMinutes = $startMinutes + $_[1];
-        $newHours = int($endMinutes / 60);
-        $newMinutes = $endMinutes % 60;
-        $newFormatted = sprintf("%d:%02d", $newHours, $newMinutes);
+        my @startTime = split(":", $_[0]);
+        my $startMinutes = $startTime[0] * 60 + $startTime[1];
+        my $endMinutes = $startMinutes + $_[1];
+        my $newHours = int($endMinutes / 60);
+        my $newMinutes = $endMinutes % 60;
+        my $newFormatted = sprintf("%d:%02d", $newHours, $newMinutes);
         return $newFormatted;
+    }
+}
+
+# expects a time string formatted HH24:MM
+sub timeToMinutes
+{
+    my $len = @_;
+    if ($len == 1) {
+        my @theTime = split(":", $_[0]);
+        my $minutes = $theTime[0] * 60 + $theTime[1];
+        return $minutes;
+    }
+}
+
+# expects an integer
+sub minutesToTime
+{
+    my $len = @_;
+    if ($len == 1) {
+        my $totalMinutes = $_[0];
+        my $minutes = sprintf("%02d", $totalMinutes % 60);
+        my $hours = int($totalMinutes / 60);
+        return "$hours:$minutes";
+    }
+}
+
+sub currentTimeFormatted
+{
+    (my $currentMinutes, my $currentHours) = (localtime)[1,2];
+    return $currentHours . ":" . sprintf("%02d", $currentMinutes);
+}
+
+# expects an integer which is the amount of minutes logged
+sub startTimeInMinutes
+{
+    my $currentTimeInMinutes = timeToMinutes(currentTimeFormatted());
+    my $rounded = nearest15Minutes($currentTimeInMinutes);
+    return $rounded - $_[0];
+}
+
+# expects an integer
+sub nearest15Minutes
+{
+    my $len = @_;
+    if ($len == 1) {
+        my $remainder = $_[0] % 15;
+        my $basetime = $_[0] - $remainder;
+        if ($remainder > 7) {
+            return $basetime + 15;
+        }
+        else {
+            return $basetime;
+        }
     }
 }
 
 sub to12HourTime
 {
-    @time = split(":", $_[0]);
+    my @time = split(":", $_[0]);
+    my $suffix;
     if ($time[0] >= 12) {
-        $time[0] -= 12;
+        if ($time[0] > 12) { $time[0] -= 12; }
         $suffix = "PM";
     }
     else {
@@ -93,5 +161,7 @@ sub to12HourTime
         }
         $suffix = "AM";
     }
+    chomp($time[1]);
+    sprintf("%02d", $time[1]);
     return join(":", @time)." $suffix";
 }
